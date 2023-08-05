bd3de91c2eddade394345e1e5dcd5ba0b10d672b
SynicalMX <70176240+SynicalMX@users.noreply.github.com>
2023-08-05 14:01:49 -0400

    add parseImage

--- src/cc/PaintUtils.hx
+++ src/cc/PaintUtils.hx
@@ -8,6 +8,7 @@ typedef Image = Table<Int, Table<Int, Int>>;
 @:native("paintutils")
 extern class PaintUtils {
 	public static function loadImage(path:String):Image;
+    public static function parseImage(image:String):Image;
 	public static function drawImage(image:Image, x:Int, y:Int):Void;
 	public static function drawPixel(x:Int, y:Int, ?color:Color):Void;
 	public static function drawLine(startX:Int, startY:Int, endX:Int, endY:Int, ?color:Color):Void;
